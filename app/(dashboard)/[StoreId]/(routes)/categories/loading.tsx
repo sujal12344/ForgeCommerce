60b884baa5edde5f14f08e2fb5cd83b51f6c8021
sujal <sujalkesharwani220@gmail.com>
2025-12-29 02:06:25 +0530

    feat: implement Loading component for consistent loading states across routes

--- app/(dashboard)/[StoreId]/(routes)/categories/loading.tsx
+++ app/(dashboard)/[StoreId]/(routes)/categories/loading.tsx
@@ -1,16 +1,3 @@
-"use client";
-
-import { Loader } from "@/components/ui/loader";
-
-const Loading = () => {
-  return (
-    <div className="flex space-x-2 h-full w-full items-center justify-center">
-      <Loader />
-      <p className="animate-pulse text-gray-600 text-lg">
-        Preparing awesomeness...
-      </p>
-    </div>
-  );
-};
+import { Loading } from "@/components/ui/loading";
 
 export default Loading;
